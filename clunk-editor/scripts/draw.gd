c3f154e0a187071b71fd78b281d5bf0b3ea705ee
jeffrey m <48529067+jeffinitup@users.noreply.github.com>
2025-01-18 20:23:29 -0500

    polygon picking

--- clunk-editor/scripts/draw.gd
+++ clunk-editor/scripts/draw.gd
@@ -9,12 +9,42 @@ func _draw() -> void:
 	var time := Time.get_ticks_usec() / 1000000.0
 	var fac := 0.5 + (sin(time * 2) * 0.25)
 	
+	# Draw existing polygons
 	for polygon in editor.poly_list:
+		# If polygon has less than three points, dont draw
+		if polygon.points.size() < 3:
+			continue
 		draw_polygon(polygon.points, polygon.colors)
+		
+		# Draw additional border if polygon is hovered over
+		if polygon.focused:
+			var lines := PackedVector2Array(polygon.points)
+			lines.append(lines[0])
+			draw_polyline(lines, Color.RED, 2.0)
+	
+	# Draw in progress polygon
 	if editor.poly_cur:
 		var poly := PackedVector2Array(editor.poly_cur.points)
 		poly.append(get_global_mouse_position())
+		
+		var line := PackedVector2Array(poly)
+		line.append(line[0])
+		draw_polyline(line, Color.BLUE)
+		
+		# Stop drawing if less than three points
+		if poly.size() < 3:
+			return
+		
 		draw_colored_polygon(poly, Color(Color.WHITE, fac))
 
 func _process(delta: float) -> void:
 	queue_redraw()
+
+func _physics_process(delta : float) -> void:
+	# Check for mouse overlap
+	var mpos := get_global_mouse_position()
+	for polygon in editor.poly_list:
+		if Geometry2D.is_point_in_polygon(mpos, polygon.points):
+			polygon.focused = true
+			continue
+		polygon.focused = false
--- clunk-editor/scripts/editor.gd
+++ clunk-editor/scripts/editor.gd
@@ -23,13 +23,30 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.button_index == MOUSE_BUTTON_MASK_RIGHT && event.is_pressed():
 			if !poly_cur:
 				return
-			poly_list.append(poly_cur)
-			poly_cur = null
+			commit_polygon()
 			
 		## Delete polygon
 
+func commit_polygon() -> void:
+	# If collision triangulation fails, we have a bad polygon
+	if Geometry2D.triangulate_polygon(poly_cur.points).is_empty():
+		push_warning("Bad triangulation! - " + str(poly_cur.rid))
+		poly_cur = null
+		return
+
+	# If collision decomposition fails, we also have a bad polygon
+	if Geometry2D.decompose_polygon_in_convex(poly_cur.points).is_empty():
+		push_warning("Bad decompisition! - " + str(poly_cur.rid))
+		poly_cur = null
+		return
+
+	# Add to list
+	poly_list.append(poly_cur)
+	poly_cur = null
+
 class Polygon extends Resource:
 	var rid : int
+	var focused : bool = false
 	var points := PackedVector2Array()
 	var colors := PackedColorArray()
 	
